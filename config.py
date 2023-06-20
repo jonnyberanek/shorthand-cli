e0558c423e4c9946111031712bf58504275e1057
Jonny Beranek <jonnyberanek@gmail.com>
2023-06-20 14:45:58 -0400

    Added dist template and made config dynamic

--- config.py
+++ config.py
@@ -1,2 +1,16 @@
-commandDir = "C:\\Users\\jberanek\\Projects\\Tools\\utility-scripts\\shorthand\\commands"
-shellProgram = "C:\\Program Files\\Git\\bin\\bash.exe"
+import sys
+from yaml import load
+from yaml.loader import Loader
+from os.path import dirname, join, isabs
+
+SETTINGS_FILE = "./settings.yml"
+
+rootDir = dirname(sys.argv[0])
+
+def joinToRoot(path: str):
+  return path if isabs(path) else join(rootDir, path)
+
+config = load(open(joinToRoot(SETTINGS_FILE)), Loader)
+
+commandDir = joinToRoot(config["commandDir"])
+shellProgram = joinToRoot(config["shellProgram"])
